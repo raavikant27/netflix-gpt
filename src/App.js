9bec3ec5e3347edebbbfb1a657f65f4e41786dc6
raavikant27 <sravikant302@gmail.com>
2023-12-23 11:56:56 +0530

    cra & tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Ravikant singh</h1>
+    <div className="text-3xl front-bold text-green">
+      <h1 className="text-3xl font-bold text-green-600">Ravikant singh</h1>
     </div>
   );
 }
